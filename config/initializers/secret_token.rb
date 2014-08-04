076a9bc835616e2efa5244e750f854016def6881
Michael Zaccari <michael.zaccari@accelecon.com>
2014-08-04 17:20:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeedTest::Application.config.secret_token = '02576d3aa6e2f6c9fb058c230ca047b6ebf525b51f1454f42cdb81b37c456ce061eb6a39f8fc0b80b50c1ec0eb2f09c676d4b3e6144536aa54532145b28b1f22'
